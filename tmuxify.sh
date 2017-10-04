9fa1809d6a059d22cd8dcf944850a51d853bf3a1
Anatoly Ostrovsky <megatolya@yandex-team.ru>
2017-10-04 10:52:00 +0300

    💂🏻

---
+++ tmuxify.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cur_dirname=$(basename $PWD)
+dirname=${1:-$cur_dirname}
+
+sessions=$(tmux list-sessions -F "#{session_name}" 2>/dev/null)
+new_session=true
+
+for session in $(tmux list-sessions -F "#{session_name}"); do
+    if [ $session = $dirname ]
+    then
+        new_session=false
+    fi
+done
+
+if [ $new_session = true ]
+then
+    tmux new-session -s $dirname
+else
+    tmux attach -t ${dirname}
+fi
