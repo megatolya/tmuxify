9ea83d62b9d354b25a59be6f87feac880e4de911
Anatoly Ostrovsky <megatolya@yandex-team.ru>
2017-10-13 12:43:54 +0300

    🦇

---
+++ install.sh
@@ -0,0 +1,4 @@
+curl -fsSL https://raw.githubusercontent.com/megatolya/tmuxify/master/tmuxify.sh > /usr/local/bin/tmuxify
+chmod +x /usr/local/bin/tmuxify
+echo "tmuxify installed'
+"try it: tmuxify -h"
